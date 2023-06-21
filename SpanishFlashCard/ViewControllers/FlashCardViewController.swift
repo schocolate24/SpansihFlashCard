76715f9e68fb03d74d0f87303f9b7eb725b3a3ab
Shoko Nakaki <schoconakaki24@gmail.com>
2023-06-21 11:53:47 +0900

    TabBarControllerとViewControllersの作成

---
+++ SpanishFlashCard/ViewControllers/FlashCardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FlashCardViewController.swift
+//  SpanishFlashCard
+//
+//  Created by Shoko Nakaki on 2023/06/21.
+//
+
+import UIKit
+
+class FlashCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
